dd9b1cfb13438d74582f47e230408e2c3ef8398a
Emily Ann Ediger <dje4321@gmail.com>
2018-03-23 01:14:39 -0500

    Removed some debug code

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import function, menu, classes
 from random import randint
 
 print('Welcome to the DANGERZONE!')
-#player = classes.Player(input('Please enter your name: '))
-player = classes.Player('Emily')
+player = classes.Player(input('Please enter your name: '))
 enemys = function.randEnemy(3)
-
-# Test Values
-player.health = 1000
-player.stamina = 5
 
 menu.mainLoop(player,enemys)
